8fa6a6b4667ede1a47f632f1cbf16ea1c04e83e7
AlbeCamp21 <luis.alanya.c@uni.pe>
2025-09-23 22:07:05 -0500

    cambios para stash

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ print('Hello World-actualiado en main')
 def greet():
     print('Error corregido en la función')
 
-greet()print('Cherry pick')
+greet()
+
+print('Cherry pick')
+
 Este cambio lo hago
+
+Este cambio está en el stash
+
